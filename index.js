bde3e7bc378a9189c8cca8c8c7032c9f2e772c02
Rojoo05 <vrojotirado@gmail.com>
2026-04-27 10:16:28 -0700

    Endpoints dinamicos GET por ID para alumnos y materias

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
+
+
+
 app.get('/alumnos', async (req, res) => {
   try {
     const resultado = await pool.query('SELECT * FROM alumno');
@@ -55,8 +58,11 @@ app.post('/materias', async (req, res) => {
   try {
     const { nombre, semestre, creditos } = req.body;
 
-    if (!nombre || !semestre || !creditos) {
-      return res.status(400).json({ error: 'Faltan campos obligatorios (nombre, semestre, creditos)' });
+
+    if (!nombre || semestre == null || creditos == null) {
+      return res.status(400).json({
+        error: 'Todos los campos son obligatorios'
+      });
     }
 
     const resultado = await pool.query(
@@ -68,9 +74,55 @@ app.post('/materias', async (req, res) => {
       mensaje: 'Materia insertada correctamente',
       materia: resultado.rows[0]
     });
+
   } catch (error) {
     console.error('Error al insertar materia:', error);
     res.status(500).json({ error: 'Error al insertar la materia' });
+  }
+});
+
+app.get('/alumnos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+  if (isNaN(id)) {
+  return res.status(400).json({ error: 'El id debe ser numérico' });
+}
+    const resultado = await pool.query(
+      'SELECT * FROM alumno WHERE id = $1',
+      [id]
+    );
+
+    if (resultado.rows.length === 0) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    res.json(resultado.rows[0]);
+  } catch (error) {
+    console.error('Error al consultar usuario:', error);
+    res.status(500).json({ error: 'Error al obtener el usuario' });
+  }
+});
+
+app.get('/materias/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (isNaN(id)) {
+    return res.status(400).json({ error: 'El id debe ser numérico' });
+}
+    const resultado = await pool.query(
+      'SELECT * FROM materia WHERE id = $1',
+      [id]
+    );
+
+    if (resultado.rows.length === 0) {
+      return res.status(404).json({ error: 'Materia no encontrada' });
+    }
+
+    res.json(resultado.rows[0]);
+  } catch (error) {
+    console.error('Error al consultar materia:', error);
+    res.status(500).json({ error: 'Error al obtener la materia' });
   }
 });
 
